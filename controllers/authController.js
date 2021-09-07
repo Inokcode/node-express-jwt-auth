af21ac1b388bc7422bc9dfcf7b7ad025923524c1
Inokcode <regilearn@gmail.com>
2021-09-07 16:19:21 +0530

    user model added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,26 @@
-module.exports.signup_get = (res, res) => {
+const User = require('../models/User');
+
+module.exports.signup_get = (req, res) => {
   res.render('signup');
 };
-module.exports.login_get = (res, res) => {
+module.exports.login_get = (req, res) => {
   res.render('login');
 };
-module.exports.signup_post = (res, res) => {
-  res.send(' new signup');
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
+  // console.log(email, password);
+  // res.send(' new signup');
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send('error,user not created');
+  }
 };
-module.exports.login_post = (res, res) => {
+module.exports.login_post = async (req, res) => {
+  //   console.log(req.body);
+  const { email, password } = req.body;
+  console.log(email, password);
   res.send('user login');
 };
